e14e03e9dbc61d3966b854212aa10be0f6b9a9a0
Nicolas Hiillos <nicolashiillos@gmail.com>
2017-10-13 14:39:19 +0300

    l

--- src/main/java/tikape/runko/database/PirteloDao.java
+++ src/main/java/tikape/runko/database/PirteloDao.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tikape.runko.database;
 
 import java.sql.Connection;
@@ -13,10 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 import tikape.runko.domain.Pirtelo;
 
-/**
- *
- * @author strajama
- */
 public class PirteloDao implements Dao<Pirtelo, Integer>{
     
     private Database database;
