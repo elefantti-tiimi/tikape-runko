9f20f343b96d8ba68e700e2631178f186ff826ad
Nicolas Hiillos <nicolashiillos@gmail.com>
2017-10-13 13:39:43 +0300

    pieniä korjauksia

--- src/main/java/tikape/runko/domain/Aines.java
+++ src/main/java/tikape/runko/domain/Aines.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tikape.runko.domain;
 
-/**
- *
- * @author strajama
- */
 public class Aines {
     
-        private Integer id;
+    private Integer id;
     private String nimi;
 
     public Aines(Integer id, String nimi) {
